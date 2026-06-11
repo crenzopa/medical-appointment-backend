24d32bf429b8e17764f9ea3e1e4cddd6498b9652
Carlos Renzo Paz Ascate <carlosr.paz@softtekprojects.com>
2026-06-11 12:51:06 -0600

    feat: correction to medical appointment creation

--- src/presentation/handlers/createAppointment.handler.ts
+++ src/presentation/handlers/createAppointment.handler.ts
@@ -4,18 +4,18 @@ import { SNSClient } from "@aws-sdk/client-sns";
 import { DynamoAppointmentRepository } from "../../infrastructure/dynamodb/DynamoAppointmentRepository";
 import { CreateAppointmentUseCase } from "../../application/use-cases/CreateAppointmentUseCase";
 
-const sns = new SNSClient({});
-const dynamo = new DynamoDBClient({});
-
-const repo = new DynamoAppointmentRepository(dynamo);
-
-const useCase = new CreateAppointmentUseCase(
-  repo,
-  sns,
-  process.env.SNS_TOPIC_ARN!
-);
-
 export const handler = lambdaInterceptor(async (event: any) => {
+  const sns = new SNSClient({});
+  const dynamo = new DynamoDBClient({});
+
+  const repo = new DynamoAppointmentRepository(dynamo);
+
+  const useCase = new CreateAppointmentUseCase(
+    repo,
+    sns,
+    process.env.SNS_TOPIC_ARN as string
+  );
+
   const body = JSON.parse(event.body);
 
   const result = await useCase.execute(body);
